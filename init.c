583373128d8847eabab47161fc07c9ddcacb8d50
leiting <leit@mail.ustc.edu.cn>
2018-04-22 16:38:01 +0800

    add environment variable and the error input is indicated

--- init.c
+++ init.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+
+extern char ** environ;
 
 int main() {
     /* 输入的命令行 */
@@ -12,8 +15,8 @@ int main() {
     while (1) {
         /* 提示符 */
         printf("# ");
-        fflush(stdin);
-        fgets(cmd, 256, stdin);
+        fflush(stdin);  //清空输入缓冲区
+        fgets(cmd, 256, stdin);   //读入单行指令
         /* 清理结尾的换行符 */
         int i;
         for (i = 0; cmd[i] != '\n'; i++)
@@ -36,18 +39,48 @@ int main() {
 
         /* 内建命令 */
         if (strcmp(args[0], "cd") == 0) {
-            if (args[1])
-                chdir(args[1]);
+            char wd[4096];
+	    if (chdir(args[1])>=0){
+	    printf("Current dir is:\n");
+            puts(getcwd(wd, 4096));
+ 	    }
+ 	    else
+            {
+            printf("Error path!\n");
+            }
             continue;
         }
         if (strcmp(args[0], "pwd") == 0) {
             char wd[4096];
+	    printf("Current dir is:\n");
             puts(getcwd(wd, 4096));
             continue;
         }
-        if (strcmp(args[0], "exit") == 0)
-            return 0;
-
+        if (strcmp(args[0], "export") == 0){
+	    char *PATH_NAME;
+ 	    PATH_NAME=(char*)malloc(sizeof(char)*strlen(args[1]));
+	    strcpy(PATH_NAME,args[1]);
+	    if(putenv(PATH_NAME)!=0){
+	    printf("Error!\n");
+	    }
+            continue;
+	}
+        if (strcmp(args[0], "echo") == 0){
+	    char *p = getenv(args[1]);
+	    if(p)
+		printf("%s\n",p);
+	    else
+		printf("Error Path\n");
+            continue;
+	}
+        if (strcmp(args[0], "unset") == 0){
+	    unsetenv(args[1]);
+            continue;
+	}
+        if (strcmp(args[0], "exit") == 0){
+	    exit(0);
+            return 1;
+	}
         /* 外部命令 */
         pid_t pid = fork();
         if (pid == 0) {
